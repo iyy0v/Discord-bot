3cc58c376a4c2a1f333c01b5febf512bc0a013e5
Ayoub <56415787+iyy0v@users.noreply.github.com>
2022-01-19 20:34:47 +0100

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -1,1 +1,20 @@
-import discord
+import discord, os
+
+
+token = ""
+server_name = ""
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	for server in client.guilds:
+		if server.name == server_name:
+			print(
+        		f'{client.user} is connected to the following servers:\n'
+       			f'{server.name} (id: {server.id})'
+    		)
+    		break
+
+
+client.run(token)
