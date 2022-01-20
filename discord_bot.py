dd64f825fa06465f3ff59723737407ff4be29596
Ayoub <56415787+iyy0v@users.noreply.github.com>
2022-01-20 15:22:47 +0100

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -3,8 +3,12 @@ import discord, os
 
 token = ""
 server_name = ""
+members = []
 
-client = discord.Client()
+# you must enable the "SERVER MEMBERS INTENT" Privileged Gateway Intent under the bot settings in the Discord developer portal
+intents = discord.Intents.default() 
+intents.members = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
@@ -14,7 +18,17 @@ async def on_ready():
         		f'{client.user} is connected to the following servers:\n'
        			f'{server.name} (id: {server.id})'
     		)
-    		break
+			break
 
+	print("\n Users : ")
+	for member in server.members:
+		if(member.bot == False):
+			print(f"  {member.name}#{member.discriminator} (Nickname: {member.nick})")
+
+
+	print("\n Bots : ")
+	for member in server.members:
+		if(member.bot == True):
+			print(f"  {member.name}#{member.discriminator} (Nickname: {member.nick})")
 
 client.run(token)
