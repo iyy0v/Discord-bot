3f9a55ae1ed19b9848b5e700d2f1b9e004429b27
Ayoub <56415787+iyy0v@users.noreply.github.com>
2022-01-23 23:15:45 +0100

    Update
    Added messages delete and better permissions check for some commands

--- discord_bot.py
+++ discord_bot.py
@@ -3,6 +3,8 @@ import discord, os, random
 
 token = "" 		# <--- put the bot's token here
 server_name = ""	# <--- put the server's name here
+prefix = '$'		# <--- put the bot's command prefix that you like ('$' by default)
+
 members = []
 
 
@@ -33,14 +35,30 @@ async def on_message(message):
 	if(logging): 
 		logged_messages.append(f'#{message.channel.name}/{message.author.name}#{message.author.discriminator}: {message.content}')
 
-
-		if(message.content[0] == '$'):
+	# COMMANDS SECTION
+	if(message.content[0] == prefix):
 		if(message.content[1:].lower() in greetings):
 			greeting = greetings[random.randint(0,len(greetings)-1)].capitalize()
 			await message.reply(greeting + " !")
 		
 		elif(message.content[1:].lower() == 'twerk'):
 			await message.channel.send("NO")
+
+		# Return the invite link for the bot
+		elif(message.content[1:].lower() == 'invite'):
+			await message.channel.send(discord.utils.oauth_url(client_id))
+
+		# Delete message
+		elif(message.content[1:8].lower() == 'delete '):
+			permission = False
+			for role in message.author.roles:
+				if role.permissions.manage_messages == True or role.permissions.administrator == True:
+					permission = True
+			if permission:
+				if(message.content[8:].isdigit()):
+					await message.channel.purge(limit=int(message.content[8:])+1)
+				else:
+					await message.channel.send("Invalid number !")
 
 		# Messages logging commands
 		elif(message.content[1:].lower() == 'log start'):
@@ -66,13 +84,13 @@ async def on_message(message):
 			await message.channel.send("Messages logs were deleted.")
 		#############################################################
 
-		# Return the invite link for the bot
-		elif(message.content[1:].lower() == 'invite'):
-			await message.channel.send(discord.utils.oauth_url(client_id))
-
 		# kick a member 
 		elif(message.content[1:6].lower() == 'kick '):
-			if(message.author.top_role.permissions.administrator == True):
+			permission = False
+			for role in message.author.roles:
+				if role.permissions.ban_members == True or role.permissions.administrator == True:
+					permission = True
+			if permission:
 				member_kicked = discord.utils.get(message.guild.members, name=message.content[6:-5], discriminator=message.content[-4:])
 				if(member_kicked == None):
 					await message.channel.send(f"Incorrect name: {message.content[6:-5]}#{message.content[-4:]}")
@@ -80,6 +98,23 @@ async def on_message(message):
 					await message.channel.send(f"{member_kicked.name}#{member_kicked.discriminator} was KICKED !")
 			else:
 				await message.channel.send("You don't have the permission to do that.")
+		
+		# ban a member 
+		elif(message.content[1:5].lower() == 'ban '):
+			permission = False
+			for role in message.author.roles:
+				if role.permissions.ban_members == True or role.permissions.administrator == True:
+					permission = True
+			if permission:
+				member_banned = discord.utils.get(message.guild.members, name=message.content[5:-5], discriminator=message.content[-4:])
+				if(member_banned == None):
+					await message.channel.send(f"Incorrect name: {message.content[5:-5]}#{message.content[-4:]}")
+				else:
+					await message.channel.send(f"{member_banned.name}#{member_banned.discriminator} was BANNED !")
+			else:
+				await message.channel.send("You don't have the permission to do that.")
+		
+
 		#############################################################
 		
 		else:
